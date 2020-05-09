0af676a26f8aa1952352153a2274e9ca097402de
hyunjun <guswns1659@gmail.com>
2020-05-09 21:04:16 +0900

    [Test] index 테스트 중

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,14 @@
-var store = []
+var store = [{
+    "title": "Edge Case: Nested and Mixed Lists",
+    "excerpt": "Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...",
+    "categories": ["Edge Case"],
+    "tags": ["content", "css", "edge case", "lists", "markup"],
+    "url": "http://localhost:4000/edge%20case/edge-case-nested-and-mixed-lists/",
+    "teaser": null
+}, {
+    "title": "Edge Case: Many Tags",
+    "excerpt": "This post has many tags.  ", "categories": ["Edge Case"],
+    "tags": ["8BIT", "alignment", "Articles", "captions", "categories", "chat", "comments", "content", "css", "dowork", "edge case", "embeds", "excerpt", "Fail", "featured image", "FTW", "Fun", "gallery", "html", "image", "Jekyll", "layout", "link", "Love", "markup", "Mothership", "Must Read", "Nailed It", "Pictures", "Post Formats", "quote", "standard", "Success", "Swagger", "Tags", "template", "title", "twitter", "Unseen", "video", "YouTube"],
+    "url": "http://localhost:4000/edge%20case/edge-case-many-tags/",
+    "teaser": null
+}]
